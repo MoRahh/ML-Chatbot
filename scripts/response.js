b8d1d439275dd84734785ed102fe63425c8b015d
Mohammed Aburahma <91658818+MoRahh@users.noreply.github.com>
2022-07-03 08:31:13 +0300

    Update response.js

--- scripts/response.js
+++ scripts/response.js
@@ -15,6 +15,10 @@ function getBotResponse(input) {
         return "Talk to you later!";
     } else if(input == "Heart clicked!") {
         return "Thank You!";
+    } else if(input == "who are you") {
+        return "I'm Mohammed, Here to help you with anything.";
+    } else if(input == "how old are you") {
+        return "I dont have a specific age, I'm just a bot but i was made on 2nd of july by Mohammed";
     } else {
         return "Try asking something else!";
     }
