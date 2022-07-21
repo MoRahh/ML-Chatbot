435d8b26d2de726e7b35cc2771b3bf2226de8e24
MoRahh <moodyaburahma@gmail.com>
2022-07-21 16:55:20 +0300

    Added more responses

--- scripts/response.js
+++ scripts/response.js
@@ -13,14 +13,18 @@ function getBotResponse(input) {
         return "Hello there!";
     } else if (input == "goodbye") {
         return "Talk to you later!";
-    } else if(input == "Heart clicked!") {
+    } else if (input == "Heart clicked!") {
         return "Thank You!";
-    } else if(input == "who are you") {
+    } else if (input == "who are you") {
         return "I'm Mohammed, Here to help you with anything.";
-    } else if(input == "how old are you") {
+    } else if (input == "how old are you") {
         return "I dont have a specific age, I'm just a bot but i was made on 2nd of july by Mohammed";
-    } else if(input == "") {
+    } else if (input == "") {
         return "Please enter a valid message";
+    } else if (input == "How are you") {
+        return "Doin great buddy wbu?";
+    } else if (input == "good") {
+        return "good to know";
     } else {
         return "Try asking something else!";
     }
